a52782433220216ec7cf9ead18141a82718014b9
Altin Hysa <altinhysa84@gmail.com>
2025-01-22 11:12:54 +0100

    fix: revert base url

--- src/services/robodev/robodev-rest.client.ts
+++ src/services/robodev/robodev-rest.client.ts
@@ -1,3 +1,3 @@
 import { RestClient } from "../../utils/rest/rest.client"
 
-export const robodevRestClient = new RestClient({ config: { baseURL: "http://localhost:3000/api" } })
+export const robodevRestClient = new RestClient({ config: { baseURL: "https://cline.robodev.com/api" } })
